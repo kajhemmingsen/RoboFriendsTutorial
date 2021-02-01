29fadf085b37c2b0d2f67d8d16d5cdaf8b3e3c4f
kajhemmingsen <kaj.hemmingsen.85@gmail.com>
2021-02-01 11:45:49 -0500

    Initial commit for small tutorial application using react

---
+++ src/Containers/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import Cardlist from '../Components/Cardlist';
+import Searchbox from '../Components/Searchbox';
+import Scroll from '../Components/Scroll';
+
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: [],
+            searchfield: ''
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response=> response.json())
+        .then(users => this.setState({ robots: users}));   
+    }
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value});
+        
+    }
+
+    render() {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+            })
+        if (!this.state.robots.length) {
+            return <h1>Loading</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1'>RoboFriends</h1>
+                    <Searchbox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <Cardlist robots={filteredRobots}/>
+                    </Scroll>
+                    
+                </div>
+        );}}
+}
+
+export default App;
